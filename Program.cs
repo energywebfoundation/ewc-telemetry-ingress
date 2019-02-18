bb3d05a9974429fc4d9558d890849280a9b5a81a
Markus Keil <markus.keil@slock.it>
2019-02-18 10:07:57 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace telemetry_ingress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
