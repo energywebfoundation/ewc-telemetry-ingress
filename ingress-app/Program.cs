d6f2163a31b444fc60d4824a8c2fc5619dae403d
Markus Keil <markus.keil@slock.it>
2019-02-28 09:11:36 +0100

    read influx config from env

--- ingress-app/Program.cs
+++ ingress-app/Program.cs
@@ -1,48 +1,52 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace webapi
 {
     public class Program
     {
-        private static string GetConfigFromEnvironment(string name, string defaultValue)
-        {
-            string value = Environment.GetEnvironmentVariable(name);
-            return String.IsNullOrWhiteSpace(value) ? defaultValue : value;
-        }
-
-        
         public static void Main(string[] args)
         {
-
-            string certificatePath = Path.Combine(GetConfigFromEnvironment("TELEMETRY_INTERNAL_DIR", "./"),"telemetry-ingress.pfx");
+            
+            // build config
+            string envName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{envName}.json", true)
+                .AddEnvironmentVariables("TELEMETRY")
+                .AddInfluxConfigFromEnvironment()
+                .Build();
+            
+            string certificatePath = Path.Combine(config.GetValue("INTERNAL_DIR","./"),"telemetry-ingress.pfx");
             if (!File.Exists(certificatePath))
             {
                 Console.WriteLine($"Error: Unable to read certificate from {certificatePath}. File not found");
                 return;
             }
 
-            string keyPassword = GetConfigFromEnvironment("TELEMETRY_KEYPASS", String.Empty);
+            string keyPassword = config.GetValue("KEYPASS", String.Empty);
             if (String.IsNullOrWhiteSpace(keyPassword))
             {
                 Console.WriteLine($"Error: Certificate password not provided.");
                 return;
             }
-            
-            IWebHost host = WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                 .UseKestrel(options =>
-                    {
-                        options.Listen(IPAddress.Loopback, 5000);
-                        options.Listen(IPAddress.Loopback, 5001, listenOptions =>
-                        {
-                            listenOptions.UseHttps(certificatePath, keyPassword );
-                        });
-                    })
 
+
+            IWebHost host = WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(config)
+                .UseStartup<Startup>()
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Loopback, 5000);
+                    options.Listen(IPAddress.Loopback, 5001,
+                        listenOptions => { listenOptions.UseHttps(certificatePath, keyPassword); });
+                })
                 .Build();
 
             host.Run();
